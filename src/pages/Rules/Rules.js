0415722fd2b00893c9b75d7720493a1f43437175
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-08 23:25:57 +0300

    Rules page

---
+++ src/pages/Rules/Rules.js
@@ -0,0 +1,31 @@
+import {InnerHeader} from "../../components";
+
+function Rules(){
+    return (
+        <>
+            <InnerHeader title={'Rules'} />
+            <main>
+                <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+
+                    <div className="px-4 py-6 sm:px-0">
+                        <div className="h-fit">
+
+                            <div>
+                                <h3 >Something like title</h3>
+                                <p>Lorem ipsum dolor sit amet,
+                                    consectetur adipisicing elit.
+                                    Autem blanditiis commodi cum deserunt dolor eos ex,
+                                    inventore itaque mollitia nisi officia omnis quis repudiandae
+                                    sed ullam ut voluptates voluptatum. Dicta?
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+
+    )
+}
+export default Rules;
