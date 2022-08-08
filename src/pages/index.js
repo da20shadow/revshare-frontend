883244f16b8d9aa7b0e059eb7d3280af5870e0bf
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-08 23:25:15 +0300

    one import for all pages

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+export {default as Home} from './Home';
+export {default as News} from './News';
+export {default as Faq} from './FAQ';
+export {default as Rules} from './Rules';
+export {default as How} from './How';
+export {default as Contact} from './Contact';
+export {default as Login} from './Login';
+export {default as Register} from './Register';
+export {default as Account} from './Account';
+export {default as Edit} from './Edit';
+export {default as Deposit} from './Deposit';
+export {default as Withdrawal} from './Withdrawal';
+export {default as BuyShares} from './BuyShares';
+export {default as SellShares} from './SellShares';
+export {default as Marketplace} from './Marketplace';
