d8f34ec1dfe2fe3d92ea20bb05c6da51cda859a6
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-10 14:47:28 +0300

    sell shares page

--- src/pages/SellShares/SellShares.js
+++ src/pages/SellShares/SellShares.js
@@ -1,6 +1,9 @@
-import {InnerHeader} from "../../components";
+import {InnerHeader, Modal} from "../../components";
+import {useState} from "react";
 
 function SellShares () {
+    const [showModal,setShowModal] = useState(false);
+
     const btnStyle = 'mx-3 py-3 px-10 font-bold text-lg text-gray-100 border border-gray-400 bg-gradient-to-r from-orange-400 to-orange-600 rounded-md';
     const inputStyle = 'py-2 px-5 text-lg border border-gray-500 w-full rounded-md';
     const formStyle = 'mt-5 py-10 px-5 grid grid-cols-1 md:grid-cols-2 bg-gray-100 border border-gray-200';
@@ -9,9 +12,26 @@ function SellShares () {
     const editBtn = 'mx-1 px-3 py-1 text-gray-100 font-bold hover:shadow-lg hover:text-gray-200 text-sm border border-sky-700 bg-gradient-to-r from-sky-400 to-blue-600 rounded-md';
 
 
+    const orderId = '125';
+    const modalForm = (
+        <form className={'flex justify-center my-5 text-lg'}>
+            <label>
+            Change Sell Price:
+            <input className={'ml-3 py-1 px-3 border border-gray-300 bg-gray-100 rounded-md'}
+                   type="number" step={'0.0001'} placeholder={'$1.0000'}/>
+            </label>
+        </form>);
+
     return (
         <>
             <InnerHeader title={'Sell Shares'} />
+            {
+                showModal ? <Modal setShowModal={setShowModal}
+                                   title={`Edit Order ID: ${orderId}`}
+                                   btnText={'Save Changes'}
+                                   form={modalForm} /> : ''
+            }
+
             <main>
                 <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
 
@@ -20,7 +40,7 @@ function SellShares () {
 
                             <div className={'w-full md:w-3/4 mx-auto'}>
                                 <div className={boxStyle}>
-                                    <h2 className={'text-2xl text-center'}>Available Ad Shares: <strong>8569</strong></h2>
+                                    <h2 className={'text-2xl text-center'}>Available Ad Shares: <strong>569</strong></h2>
 
                                     <form className={formStyle}>
 
@@ -32,7 +52,7 @@ function SellShares () {
 
                                         <div className={'m-3'}>
                                             <label>
-                                                Shares Type:
+                                                Select Shares Type:
                                                 <select name="sharesType" className={inputStyle}>
                                                     <option value="adShares">Ad Shares</option>
                                                     <option value="tradingShares">Trading Shares</option>
@@ -42,7 +62,7 @@ function SellShares () {
 
                                         <div className={'m-3'}>
                                             <label>Sell At Price:
-                                                <input className={inputStyle} type="number" placeholder={'1'} step={'0.0001'}/>
+                                                <input className={inputStyle} type="number" placeholder={'$1.00'} step={'0.0001'}/>
                                             </label>
                                         </div>
 
@@ -71,6 +91,7 @@ function SellShares () {
                                     <table className={'w-full'}>
 
                                         <thead className={'bg-sky-700 text-white'}>
+                                            <th className={'py-3 px-5 text-lg border-r border-gray-100'}>#</th>
                                             <th className={'py-3 px-5 text-lg border-r border-gray-100'}>Shares</th>
                                             <th className={'py-3 px-5 text-lg border-r border-gray-100'}>Quantity</th>
                                             <th className={'py-3 px-5 text-lg border-r border-gray-100'}>Price</th>
@@ -79,11 +100,12 @@ function SellShares () {
 
                                         <tbody>
                                             <tr className={'hover:bg-gray-100 text-center'}>
+                                                <td className={'border border-gray-200 py-3 px-5'}>125</td>
                                                 <td className={'border border-gray-200 py-3 px-5'}>Ad Shares</td>
                                                 <td className={'border border-gray-200 py-3 px-5'}>150</td>
                                                 <td className={'border border-gray-200 py-3 px-5'}>$1.2523</td>
                                                 <td className={'border border-gray-200 py-3 px-5'}>
-                                                    <button className={editBtn}>Edit</button>
+                                                    <button onClick={()=>setShowModal(!showModal)} className={editBtn}>Edit</button>
                                                     <button className={cancelBtn}>Cancel</button>
                                                 </td>
                                             </tr>
