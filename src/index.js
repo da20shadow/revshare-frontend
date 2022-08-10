db5e160df4077317c7c99b2f21136b27bb77599d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-10 19:26:18 +0300

    added context

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './assets/style/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {ContextProvider} from "./context/ContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ContextProvider>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </ContextProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
