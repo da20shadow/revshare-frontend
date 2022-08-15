9475989adad5fdca5cc44b71acc918209de158bc
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-15 21:34:25 +0300

    Marketplace getting shares from db and display them

--- src/pages/Marketplace/Marketplace.js
+++ src/pages/Marketplace/Marketplace.js
@@ -1,39 +1,114 @@
 import {InnerHeader, Modal} from "../../components";
 import './Marketplace.css';
 import {MdArrowBackIos, MdOutlineArrowForwardIos} from "react-icons/md";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {buy, getAll} from "../../services/sharesService";
+import {useStateContext} from "../../context/ContextProvider";
 
 function Marketplace() {
-    const [showModal,setShowModal] = useState(false);
+    const {user,accountStat} = useStateContext();
+    const [shares, setShares] = useState();
+    const [totalShares, setTotalShares] = useState();
+    const [order, setOrder] = useState({});
+    const [showModal, setShowModal] = useState(false);
+
+    const [buyQuantity, setBuyQuantity] = useState(1);
+
+    useEffect(() => {
+        getAll(user.token).then(res => {
+            console.log(res.shares)
+            setShares(res.shares);
+            setTotalShares(res.total);
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [totalShares])
+
 
     const btnStyle = 'mx-3 py-1 px-5 font-bold text-sm text-gray-100 border border-gray-400 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md';
 
+    const buySharesHandler = (e) =>{
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const {quantity} = Object.fromEntries(formData);
 
-    const buySharesForm = (
+        const totalCost = (quantity * order.price) * 1.1;
+
+        if (totalCost > accountStat.balance){
+            alert('Not Enough Money!')
+            //TODO: show notification Error not enough balance!
+            return;
+        }
+        const orderOwnerId = order.userId;
+        buy(orderOwnerId,quantity,order.id,user.token).then(res =>{
+            setTotalShares(totalShares - quantity);
+            const newQuantity = order.quantity - quantity;
+            setOrder(prevState => ({
+                ...prevState,
+                quantity: newQuantity
+            }))
+            console.log('Response:',res)
+        }).catch(err => {
+            console.log('Error: ',err)
+        })
+    }
+
+    let buySharesForm = (
         <>
-            <h3 className="text-center text-lg mt-5">Available Shares in this order: <strong>200</strong></h3>
-        <form className={'flex justify-center items-center my-5 text-lg'}>
-            <label>
-                Quantity:
-                <input className={'ml-3 py-1 px-3 border border-gray-300 bg-gray-100 rounded-md w-36'}
-                       type="number" placeholder={'1'}/>
-            </label>
-            <div className={'ml-5'}>
-                Total: <strong>$12</strong>
-            </div>
-        </form>
+            <h3 className="text-center text-lg mt-5">
+                Available Shares in this order:
+                <strong> {order.quantity}</strong>
+            </h3>
+            <form id={'modalForm'} onSubmit={buySharesHandler}
+                  className={'flex justify-center items-center my-5 text-lg'}>
+                <label>
+                    Quantity:
+                    <input name={'quantity'}
+                           onChange={(e) => setBuyQuantity(() => {
+                               try {
+                                   const orderQuantity = order.quantity;
+                                   let quantity = Number(e.target.value);
+                                   if (orderQuantity < quantity) {
+                                       return orderQuantity;
+                                   } else if (e.target.value <= 0) {
+                                       return 1;
+                                   } else {
+                                       return quantity;
+                                   }
+                               } catch (err) {
+                                   console.log(err)
+                                   return 1;
+                               }
+                           })}
+                           className={'ml-3 py-1 px-3 border border-gray-300 bg-gray-100 rounded-md w-36'}
+                           type="number" placeholder={'1'} value={buyQuantity} required />
+                </label>
+                <div className={'ml-5'}>
+                    Total: <strong>${((buyQuantity * order.price) * 1.1).toFixed(4)}</strong>
+                </div>
+            </form>
         </>
     );
+    const showOrder = (orderId) => {
+        if (!shares) {
+            return;
+        }
+        const order = shares.find(o => o.id === orderId);
+        console.log(order)
+        setOrder(order);
+        setShowModal(!showModal);
+
+    }
     return (
 
         <>
             <InnerHeader title={'Marketplace'}/>
 
             {
-                 showModal ? <Modal title={'Buy Order ID: 1'}
-                                    setShowModal={setShowModal}
-                                    btnText={'Buy'}
-                                    form={buySharesForm} /> : ''
+                showModal ? <Modal title={`Buy From Order ID: ${order.id}`}
+                                   setShowModal={setShowModal}
+                                   btnText={'Buy'}
+                                   form={buySharesForm}/> : ''
             }
 
             <main>
@@ -44,12 +119,15 @@ function Marketplace() {
 
                         <div className={'px-10 py-5 mb-10 border rounded-md shadow-lg'}>
                             <div className={'bg-gray-200 rounded-md'}>
-                                <h2 className={'py-5 text-center text-2xl text-gray-700'}>Available Shares in Marketplace: <strong>254</strong></h2>
+                                <h2 className={'py-5 text-center text-2xl text-gray-700'}>
+                                    Available Shares in Marketplace:
+                                    <strong> {totalShares ? totalShares : 'Loading...'}</strong>
+                                </h2>
                             </div>
                         </div>
 
                         <div className={'flex justify-end'}>
-                            <select name="sort" className={'px-5 py-2 rounded-md'} >
+                            <select name="sort" className={'px-5 py-2 rounded-md'}>
                                 <option value="">Sort By</option>
                                 <option value="adShares">Ad Shares</option>
                                 <option value="tradingShares">Trading Shares</option>
@@ -60,42 +138,38 @@ function Marketplace() {
                             <table className={'marketplace-table'}>
 
                                 <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Type</th>
-                                        <th>Quantity</th>
-                                        <th>Price</th>
-                                        <th>Net Amount</th>
-                                        <th>Commission</th>
-                                        <th> </th>
-                                    </tr>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                    <th>Net Amount</th>
+                                    <th>Commission</th>
+                                    <th> </th>
+                                </tr>
                                 </thead>
 
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Ad Share</td>
-                                        <td>100</td>
-                                        <td>$1.2315</td>
-                                        <td>$123.15</td>
-                                        <td>$12.315</td>
-                                        <td>
-                                            <button onClick={()=>setShowModal(!showModal)} className={btnStyle} >Buy</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Ad Share</td>
-                                        <td>200</td>
-                                        <td>$1.2315</td>
-                                        <td>$246.30</td>
-                                        <td>$24.630</td>
-                                        <td>
-                                            <button onClick={()=>setShowModal(!showModal)} className={btnStyle} >Buy</button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-
+                                { shares ?
+                                    <tbody>
+                                    {shares.map(s => {
+                                        if (s.userId !== user.id){
+                                            return (
+                                                <tr key={s.id} className={'table-row'}>
+                                                <td>{s.id}</td>
+                                                <td>{s.quantity}</td>
+                                                <td>${(s.price).toFixed(4)}</td>
+                                                <td>${(s.quantity * s.price).toFixed(4)}</td>
+                                                <td>${((s.quantity * s.price) * 0.10).toFixed(2)}</td>
+                                                <td>
+                                                    <button onClick={() => showOrder(s.id)} className={btnStyle}>Buy
+                                                    </button>
+                                                </td>
+                                            </tr>)
+                                        }
+                                    })
+                                    }
+                                    </tbody>
+                                    : ''
+                                }
                             </table>
 
                         </div>
