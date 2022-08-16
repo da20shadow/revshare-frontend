327737e91664b246aed592f29caa400357b55732
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-16 22:26:26 +0300

    Logout the user if token expired

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,15 +1,27 @@
 import {InnerHeader} from "../../components";
 import './Account.css';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {useStateContext} from "../../context/ContextProvider";
+import {getAccountStat} from "../../services/userService";
 
 function Account(){
-    const {accountStat,setAccountStat} = useStateContext();
+    const {user,accountStat,setAccountStat,logoutUser} = useStateContext();
+    const redirect = useNavigate();
 
-    useEffect(()=>{
-
-    })
+    useEffect(()=> {
+        getAccountStat(user.token).then(stat =>{
+            console.log(stat)
+            setAccountStat(stat);
+        }).catch(err =>{
+            if (err.message === 'Invalid or Expired Token!'){
+                logoutUser();
+                setTimeout(()=>{
+                    redirect('/login')
+                },1000)
+            }
+        })
+    },[])
 
     return (
         <>
@@ -21,10 +33,14 @@ function Account(){
                     <div className="px-4 py-6 sm:px-0">
                         <div className="h-fit">
 
+                            <h2 className={'text-center text-2xl mb-10'}>
+                                Welcome <strong>{user.username}</strong>!
+                            </h2>
+
                             <div className={'my-5 flex flex-wrap justify-around'}>
 
                                 <div className={'account-box'}>
-                                    <h3>$<span>{accountStat.balance}</span></h3>
+                                    <h3>$<span>{accountStat ? Number(accountStat.balance).toFixed(2) : 'Loading'}</span></h3>
                                     <p>Account Balance</p>
                                     <div>
                                         <Link to={'/deposit'} className={'buy-btn'}>Add Funds</Link>
@@ -33,16 +49,16 @@ function Account(){
                                 </div>
 
                                 <div className={'account-box'}>
-                                    <h3>{accountStat.shares}</h3>
+                                    <h3>{accountStat ? accountStat.shares : 'Loading...'}</h3>
                                     <p>Total Shares</p>
                                     <div>
-                                        <Link to={'/buy-shares'} className={'buy-btn'}>Buy</Link>
+                                        <Link to={'/marketplace'} className={'buy-btn'}>Buy</Link>
                                         <Link to={'/sell-shares'} className={'sell-btn'}>Sell</Link>
                                     </div>
                                 </div>
 
                                 <div className={'account-box'}>
-                                    <h3>$<span>{accountStat.withdrawals}</span></h3>
+                                    <h3>$<span>{accountStat ? Number(accountStat.withdrawals).toFixed(2) : 'Loading...'}</span></h3>
                                     <p>Total Withdrawals</p>
                                     <div>
                                         <Link to={'/withdrawal'} className={'sell-btn'}>History</Link>
