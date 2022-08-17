cfe9bea7bfd5c2f8716ea1f06459e4798a4ee1f1
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-17 22:00:28 +0300

    added requester for user referrals

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,24 +1,26 @@
-import {get,post, put, del} from "./requester";
-import {getUser} from "../utils/utils";
+import {get, post, put, del} from "./requester";
 
 const path = 'users/'
-const token = getUser() ? getUser().token : '';
 
 /** -------------GET------------- **/
-export const getAccountStat = () =>{
+export const getAccountStat = (token) => {
     return get(path,token);
+}
+
+export const getReferrals = (token) => {
+    return get(`${path}referrals`,token);
 }
 
 /** -------------POST------------- **/
 export const login = (userData) => {
-    return post(path,userData);
+    return post(path, userData);
 }
 export const register = (userData) => {
-    return post(path,userData);
+    return post(path, userData);
 }
-export const update = (userData) => {
-    return put(path,userData,token);
+export const update = (userData,token) => {
+    return put(path, userData, token);
 }
-export const deleteUser = () => {
-    return del(path,token);
+export const deleteUser = (token) => {
+    return del(path, token);
 }
