da394233517700be2368ba5e2348b94c8d4014eb
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-08-31 22:35:01 +0300

    Footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,39 @@
 import './Footer.css';
 import darkLogo from '../../assets/img/dark-logo.png';
 import {Link} from "react-router-dom";
+import AdBanner from "../AdBanner";
 
 function Footer() {
     const linkStyle = 'hover:text-orange-700 px-3 text-lg'
     return (
-        <footer>
+        <>
+            <AdBanner />
 
-            <div className="footer-bg">
-                <div className={'w-full md:w-3/4 mx-auto flex justify-between items-center '}>
-                    <img src={darkLogo} alt=""/>
-                    <div className={'text-center text-gray-400'}>
-                        <Link className={linkStyle} to={'/'}>Home</Link>
-                        <span>|</span>
-                        <Link className={linkStyle} to={'/how'}>How it Works</Link>
-                        <span>|</span>
-                        <Link className={linkStyle} to={'/news'}>News</Link>
-                        <span>|</span>
-                        <Link className={linkStyle} to={'/faq'}>FAQ</Link>
-                        <span>|</span>
-                        <Link className={linkStyle} to={'/contact'}>Support</Link>
+            <footer>
+
+                <div className="footer-bg">
+                    <div className={'w-full md:w-3/4 mx-auto flex justify-between items-center '}>
+                        <img src={darkLogo} alt=""/>
+                        <div className={'text-center text-gray-400'}>
+                            <Link className={linkStyle} to={'/'}>Home</Link>
+                            <span>|</span>
+                            <Link className={linkStyle} to={'/how'}>How it Works</Link>
+                            <span>|</span>
+                            <Link className={linkStyle} to={'/news'}>News</Link>
+                            <span>|</span>
+                            <Link className={linkStyle} to={'/faq'}>FAQ</Link>
+                            <span>|</span>
+                            <Link className={linkStyle} to={'/contact'}>Support</Link>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={'bg-slate-800 py-2 w-full'}>
-                <p className={'text-center text-gray-200'}>&copy; 2022 lucrative-shares.com. All Rights Reserved</p>
-            </div>
+                <div className={'bg-slate-800 py-2 w-full'}>
+                    <p className={'text-center text-gray-200'}>&copy; 2022 lucrative-shares.com. All Rights Reserved</p>
+                </div>
 
-        </footer>
+            </footer>
+        </>
 
     )
 }
