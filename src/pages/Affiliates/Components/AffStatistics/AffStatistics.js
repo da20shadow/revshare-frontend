6ff00b2680cad2f9da156b75ea20a55490394a74
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-01 23:52:13 +0300

    Updated aff stat

--- src/pages/Affiliates/Components/AffStatistics/AffStatistics.js
+++ src/pages/Affiliates/Components/AffStatistics/AffStatistics.js
@@ -1,21 +1,29 @@
-function AffStatistics () {
+function AffStatistics ({totalPurchases,commission,totalReferrals}) {
     const statBoxStyle = 'px-10 py-5 shadow-xl rounded-md bg-gray-50 border border-gray-300';
+
+    console.log(commission)
 
     return (
         <div className={'grid grid-cols-1 md:grid-cols-3 gap-8 mt-10'}>
 
             <div className={statBoxStyle}>
-                <h3 className={'text-center text-gray-700 text-2xl font-bold'}>$3335.26</h3>
-                <p className={'text-center text-gray-600'}>Ref. Deposits</p>
+                <h3 className={'text-center text-gray-700 text-2xl font-bold'}>
+                    ${totalPurchases.toFixed(2)}
+                </h3>
+                <p className={'text-center text-gray-600'}>Ref. Purchases</p>
             </div>
 
             <div className={statBoxStyle}>
-                <h3 className={'text-center text-gray-700 text-2xl font-bold'}>$333.52</h3>
+                <h3 className={'text-center text-gray-700 text-2xl font-bold'}>
+                    ${commission.toFixed(2)}
+                </h3>
                 <p className={'text-center text-gray-600'}>Total Commission</p>
             </div>
 
             <div className={statBoxStyle}>
-                <h3 className={'text-center text-gray-700 text-2xl font-bold'}>154</h3>
+                <h3 className={'text-center text-gray-700 text-2xl font-bold'}>
+                    {totalReferrals}
+                </h3>
                 <p className={'text-center text-gray-600'}>Total Referrals</p>
             </div>
 
