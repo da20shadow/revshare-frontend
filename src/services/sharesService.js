251c6723169d562863637050cf3e219ba8d8dcbd
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-05 18:49:25 +0300

    added methods for shares requests

--- src/services/sharesService.js
+++ src/services/sharesService.js
@@ -1,4 +1,4 @@
-import {post,get} from "./requester";
+import {post, get, del, patch} from "./requester";
 
 const path = 'shares/'
 
@@ -6,8 +6,24 @@ export const publish = (orderInfo,token) => {
     return post(path,orderInfo,token);
 }
 
+export const shareDividends = (data,token) => {
+    return post('share/dividends',data,token);
+}
+
+export const update = (orderInfo,token) => {
+    return patch(path,orderInfo,token);
+}
+
+export const cancel = (orderId,token) => {
+    return del(path,{orderId},token);
+}
+
 export const buy = (orderOwnerId,quantity,orderId,token) => {
     return post(`${path}${orderId}`, {orderOwnerId,quantity},token);
+}
+
+export const getProfitHistory = (token) => {
+    return get('share/dividends/',token);
 }
 
 export const getAll = (token) => {
@@ -16,4 +32,8 @@ export const getAll = (token) => {
 
 export const getUserShares = (token) => {
     return get(`user/shares/`,token);
+}
+
+export const getSharesStat = () => {
+    return get('shares/stat');
 }
