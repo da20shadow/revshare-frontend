9866d52d0aadbd8dcf97d1a1aa753dca5c334d54
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-07 20:38:42 +0300

    Created some index files

---
+++ src/pages/AdminPanel/Components/index.js
@@ -0,0 +1,8 @@
+export {default as AdminMenu} from './AdminMenu';
+export {default as AddNews} from './AddNews';
+export {default as ManageNews} from './ManageNews';
+export {default as PendingDeposits} from './PendingDeposits';
+export {default as PendingWithdrawals} from './PendingWithdrawals';
+export {default as ShareProfit} from './ShareProfit';
+export {default as ProfitHistory} from './ProfitHistory';
+export {default as EditProcessors} from './EditProcessors';
