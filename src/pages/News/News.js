ded513e266c2945861a782747667a36e882bb892
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-08 22:33:17 +0300

    Getting news from DB

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,6 +1,20 @@
 import {InnerHeader} from "../../components";
+import {useEffect, useState} from "react";
+import {getNews} from "../../services/newsService";
 
 function News() {
+    const [news,setNews] = useState([]);
+
+    useEffect(()=>{
+        getNews().then(res => {
+            setNews(res.news)
+            console.log(res.news)
+        }).catch(err => {
+
+        })
+    },[])
+
+
     const newsBox = 'px-10 py-5 my-4 bg-gray-50 hover:bg-gray-100 hover:shadow-lg';
     const newsTitleStyle = 'w-fit text-xl text-gray-700 pb-2 font-bold hover:text-sky-700';
     const descriptionStyle = 'text-gray-600';
@@ -15,54 +29,23 @@ function News() {
                     <div className="px-4 py-6 sm:px-0">
                         <div className="h-fit">
 
-                            <div className={newsBox}>
+                            {
+                                news ? news.map(n =>
+                                        <div key={n.id} className={newsBox}>
 
-                                <h2 className={newsTitleStyle}>
-                                    <a href="#">New script with new features! </a>
-                                    | <span>23/05/2022</span>
-                                </h2>
+                                            <h2 className={newsTitleStyle}>
+                                                <a href="#">{n.title} </a>
+                                                | <span>{(n.date).substr(0,10)}</span>
+                                            </h2>
 
-                                <p className={descriptionStyle}>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    Accusamus aliquam aliquid assumenda dignissimos eligendi
-                                    enim exercitationem facere inventore necessitatibus neque,
-                                    odit perspiciatis quasi recusandae repellendus similique sint
-                                    tempora ullam voluptatem.
-                                </p>
-                                <button className={'text-gray-600 hover:text-sky-700 font-bold'}>Read More...</button>
-                            </div>
-                            <div className={newsBox}>
-
-                                <h2 className={newsTitleStyle}>
-                                    <a href="#">New script with new features! </a>
-                                    | <span>23/05/2022</span>
-                                </h2>
-
-                                <p className={descriptionStyle}>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    Accusamus aliquam aliquid assumenda dignissimos eligendi
-                                    enim exercitationem facere inventore necessitatibus neque,
-                                    odit perspiciatis quasi recusandae repellendus similique sint
-                                    tempora ullam voluptatem.
-                                </p>
-                                <button className={'text-gray-600 hover:text-sky-700 font-bold'}>Read More...</button>
-                            </div>
-                            <div className={newsBox}>
-
-                                <h2 className={newsTitleStyle}>
-                                    <a href="#">New script with new features! </a>
-                                    | <span>23/05/2022</span>
-                                </h2>
-
-                                <p className={descriptionStyle}>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    Accusamus aliquam aliquid assumenda dignissimos eligendi
-                                    enim exercitationem facere inventore necessitatibus neque,
-                                    odit perspiciatis quasi recusandae repellendus similique sint
-                                    tempora ullam voluptatem.
-                                </p>
-                                <button className={'text-gray-600 hover:text-sky-700 font-bold'}>Read More...</button>
-                            </div>
+                                            <p className={descriptionStyle}>
+                                                {(n.description).substr(0,255)}...
+                                            </p>
+                                            <button className={'text-gray-600 hover:text-sky-700 font-bold'}>Read More...</button>
+                                        </div>
+                                )
+                                    : 'No News!'
+                            }
 
                         </div>
                     </div>
