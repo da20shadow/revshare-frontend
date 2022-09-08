719b38b8374b9612662e7765a2fb70a759793673
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-09-08 22:30:27 +0300

    Added routes for adminpanel pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Footer, Header} from "./components";
 import {Route, Routes, Navigate} from "react-router-dom";
 import {
-    Account,
+    Account, AdminPanel,
     Contact,
     Deposit, DepositHistory,
     Edit,
@@ -10,15 +10,23 @@ import {
     How,
     Login, Marketplace,
     News, PromoTools,
-    Register,
+    Register, ReturnsHistory,
     Rules, SellShares,
     Withdrawal, WithdrawalHistory
 } from "./pages";
 import Affiliates from "./pages/Affiliates";
 import {useStateContext} from "./context/ContextProvider";
+import {
+    AddNews, EditProcessors,
+    ManageNews, ManageUsers,
+    PendingDeposits,
+    PendingWithdrawals,
+    ProfitHistory,
+    ShareProfit
+} from "./pages/AdminPanel/Components";
 
 function App() {
-    const {isLogged} = useStateContext();
+    const {user,isLogged} = useStateContext();
   return (
       <>
           <Header />
@@ -34,6 +42,7 @@ function App() {
               <Route path={'/deposit-history'} element={isLogged ? <DepositHistory /> : <Navigate to="/login" replace={true} />} />
               <Route path={'/withdrawal'} element={isLogged ? <Withdrawal /> : <Navigate to="/login" replace={true} />} />
               <Route path={'/withdrawal-history'} element={isLogged ? <WithdrawalHistory /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/returns-history'} element={isLogged ? <ReturnsHistory /> : <Navigate to="/login" replace={true} />} />
               <Route path={'/sell-shares'} element={isLogged ? <SellShares /> : <Navigate to="/login" replace={true} />} />
               <Route path={'/marketplace'} element={isLogged ? <Marketplace /> : <Navigate to="/login" replace={true} />} />
               <Route path={'/news'} element={<News />} />
@@ -42,6 +51,15 @@ function App() {
               <Route path={'/how'} element={<How />} />
               <Route path={'/contact'} element={<Contact />} />
               <Route path={'/ref=:id'} element={<Register />} />
+              <Route path={'/scradminpanel'} element={user.role === 1 ? <AdminPanel /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/add-news'} element={user.role === 1 ? <AddNews /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/manage-news'} element={user.role === 1 ? <ManageNews /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/manage-users'} element={user.role === 1 ? <ManageUsers /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/pending-deposits'} element={user.role === 1 ? <PendingDeposits /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/pending-withdrawals'} element={user.role === 1 ? <PendingWithdrawals /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/share-dividends'} element={user.role === 1 ? <ShareProfit /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/dividends-history'} element={user.role === 1 ? <ProfitHistory /> : <Navigate to="/login" replace={true} />} />
+              <Route path={'/scradminpanel/edit-processors'} element={user.role === 1 ? <EditProcessors /> : <Navigate to="/login" replace={true} />} />
           </Routes>
           <Footer />
       </>
